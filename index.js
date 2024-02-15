767603cf14a858517ecf715043c54bd65d0bb427
MDilshodbek <mdbek55@gmail.com>
2024-02-15 18:27:22 +0900

    Created library using object in JS

---
+++ index.js
@@ -0,0 +1,21 @@
+var library = {
+  "Law of Succes": {
+    Authore: "Napoleon Hills",
+    Status: "available",
+  },
+  "Atomic Habits": {
+    Authore: "James Clear",
+    Status: "borrowed",
+  },
+  "Zero to One": {
+    Authore: "Peter Thiel",
+    Status: "returned, available",
+  },
+};
+
+library["Think and Grow rich"] = {
+  Authore: "Napoleon Hill",
+  Status: "new book",
+};
+
+console.log(library);
